bd668a513e157c6020fe1224592fa11f58b68204
y_k_007 <yashkashyap00720@gmail.com>
2019-01-22 21:36:47 +0530

    Update TestServer.java

--- TestServer.java
+++ TestServer.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 
 public class TestServer{
-public static void main(){
+public static void main(String args[]){
 Server ob=new Server();
 ob.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 ob.startRunning();
